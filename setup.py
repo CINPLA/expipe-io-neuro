7d6e135edf243a3d623337a36b89cbebc012f53b
Milad <miladh@ibv.uio.no>
2017-03-17 17:19:46 +0100

    removed versioneer depen

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import setup
 import os
 
 from setuptools import setup, find_packages
-import versioneer
 
 
 long_description = open("README.md").read()
@@ -14,6 +13,5 @@ setup(
     name="expipe-io-neuroscience",
     packages=find_packages(),
     include_package_data=True,
-    version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass()
+    version=0.1,
 )
